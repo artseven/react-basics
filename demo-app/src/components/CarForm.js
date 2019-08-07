4380574b1462529e96668575f68f43c1b90f5b33
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-07 12:56:35 -0400

    decomposing components

---
+++ demo-app/src/components/CarForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const CarForm = ({ onSubmitCar, buttonText }) => {
+    //array destructuring syntax
+    //unpacking values from arrays or properties from objects 
+    //into distinct variables
+    //actual state data  || function to update data and re-render
+    const [ carForm, setCarForm ] = useState({
+        make: '',
+        model: '',
+        year: 1900,
+        color: '',
+        price: 0,
+    });//initial state data
+
+    //using target variable on the event object
+    //to avoid referencing e.target
+    const change = ({ target }) => {
+        setCarForm({
+            //spread operator
+            ...carForm,
+            //looks into name on the input field
+            //ternary operator to check field type
+            //to make sure number field type returns number and not a string
+            [ target.name ]: target.type === 'number' ? Number(target.value) : target.value,            
+        })
+    }
+
+    const submitCar  = () => {
+        onSubmitCar({ ...carForm });
+        //re-initializing the form
+        setCarForm({
+            make: '',
+            model: '',
+            year: 1900,
+            color: '',
+            price: 0
+        });
+    };
+
+
+
+    return <form>
+        <div>
+            <label htmlFor="make-input">Make:</label>
+            <input type="text" id="make-input" name="make"
+            value={carForm.make} onChange={change}></input>
+        </div>
+        <div>
+            <label htmlFor="model-input">Model:</label>
+            <input type="text" id="model-input" name="model"
+            value={carForm.model} onChange={change}></input>
+        </div>
+        <div>
+            <label htmlFor="year-input">Year:</label>
+            <input type="number" id="year-input" name="year"
+            value={carForm.year} onChange={change}></input>
+        </div>
+        <div>
+            <label htmlFor="color-input">Color:</label>
+            <input type="text" id="color-input" name="color"
+            value={carForm.color} onChange={change}></input>
+        </div>
+        <div>
+            <label htmlFor="price-input">Price:</label>
+            <input type="number" id="price-input" name="price"
+            value={carForm.price} onChange={change}></input>
+        </div>
+        <button type="button" onClick={submitCar}>{buttonText}</button>
+    </form>
+};
+
+CarForm.defaultProps = {
+    buttonText: 'Submit Car',
+};
+
+CarForm.propTypes = {
+    buttonText: PropTypes.string,
+    onSubmitCar: PropTypes.func.isRequired,
+};
--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -2,83 +2,28 @@ import React, { useState } from 'react';
 
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
+import { CarForm } from './CarForm';
 
 //props destructured to use just cars variable
 export const CarTool = ({ cars: initialCars }) => {
-    //array destructuring syntax
-    //unpacking values from arrays or properties from objects 
-    //into distinct variables
-    //actual state data  || function to update data and re-render
-    const [ carForm, setCarForm ] = useState({
-        make: '',
-        model: '',
-        year: 1900,
-        color: '',
-        price: 0,
-    });//initial state data
+
     //creating shallow copy of the array
     const [ cars, setCars ] = useState(initialCars.concat())
-    //using target variable on the event object
-    //to avoid referencing e.target
-    const change = ({ target }) => {
-        setCarForm({
-            //spread operator
-            ...carForm,
-            //looks into name on the input field
-            //ternary operator to check field type
-            //to make sure number field type returns number and not a string
-            [ target.name ]: target.type === 'number' ? Number(target.value) : target.value,            
-        })
-    }
 
-    const addCar = () => {
+
+    const addCar = (car) => {
         
         setCars(cars.concat({
-            ...carForm,
+            ...car,
             //creating unique id that will use highest number id + 1
             id: Math.max(...cars.map(c => c.id)) + 1
         }));
 
-        //re-initializing the form
-        setCarForm({
-            make: '',
-            model: '',
-            year: 1900,
-            color: '',
-            price: 0
-        });
     }
 
     return <>
         <ToolHeader headerText="Car Tool" />
-        <CarTable />
-        <form>
-            <div>
-                <label htmlFor="make-input">Make:</label>
-                <input type="text" id="make-input" name="make"
-                 value={carForm.make} onChange={change}></input>
-            </div>
-            <div>
-                <label htmlFor="model-input">Model:</label>
-                <input type="text" id="model-input" name="model"
-                 value={carForm.model} onChange={change}></input>
-            </div>
-            <div>
-                <label htmlFor="year-input">Year:</label>
-                <input type="number" id="year-input" name="year"
-                 value={carForm.year} onChange={change}></input>
-            </div>
-            <div>
-                <label htmlFor="color-input">Color:</label>
-                <input type="text" id="color-input" name="color"
-                 value={carForm.color} onChange={change}></input>
-            </div>
-            <div>
-                <label htmlFor="price-input">Price:</label>
-                <input type="number" id="price-input" name="price"
-                 value={carForm.price} onChange={change}></input>
-            </div>
-            <button type="button" onClick={addCar}>Add Car</button>
-        </form>
+        <CarTable cars={cars}/>
+        <CarForm buttonText="Add Car" onSubmitCar={addCar} />
     </>;
 };
