d28bc703d81e60a500c386280b60541386587ccb
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-07 10:08:36 -0400

    first component

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//custom components uppercase
+const HelloWorld = () => {
+    return React.createElement('h1', null, 'Hello World!');
+};
+
+ReactDOM.render(
+    React.createElement(HelloWorld),
+    document.querySelector('#root')
+)
